5d19b3b8717913cdc4385f65c047f15393c5cc44
SiriusSlayer <sibtainansari2003@yahoo.co.in>
2024-01-03 21:54:08 +0530

    Added the Dump of Database of movies and genres

--- IMDB_BeautifulSoup_Scraper.py
+++ IMDB_BeautifulSoup_Scraper.py
@@ -9,29 +9,28 @@ headers = {
 
 def get_genres():
     url = "/feature/genre"
+    
     r = requests.get(url=imdb + url, headers=headers)
     soup = bs(r.content, "html.parser")
     r.close()
-    #Using beautifulSoup to parse the Doc.
+    
+    # Using beautifulSoup to parse the Doc.
     first_div = soup.find("div", attrs={"class": "ipc-chip-list__scroller"})
     genres_tags = first_div.find_next("div", attrs={"class": "ipc-chip-list__scroller"})
+    # The genre tags for movies is stored in the second div
     
-    #The genre tags for movies is stored in the second div
-    genre_list = genres_tags.select(
-        "span"
-    )  
+    genre_list = genres_tags.select("span")
     # gives the list of span- tags present in the html code(The span tags contain the name of the genre).
-    web_links = genres_tags.select(
-        "a"
-    )  
+    
+    web_links = genres_tags.select("a")
     # gives the list of a - tags present in the html code(The a tags contain the link of the genre)
 
     # formating the results to get the results in str and zipping it, for further use.
-    
+
     genre_list = [genre.text for genre in genre_list]
     genre_link = [imdb + web_link["href"] for web_link in web_links]
     genre = zip(genre_list, genre_link)
-    
+
     return genre
 
 
@@ -40,27 +39,33 @@ def get_movies(genre_link):
     r = requests.get(url=genre_link, headers=headers)
     soup = bs(r.content, "html.parser")
     r.close()
-    #The movies list is stored in a ul 
-    movies_ul = soup.find("ul", attrs={"role": "presentation"})
-    if movies_ul ==None:
-        return {}
-    #h3 tags contain the names of the movies
-    movies_list = movies_ul.select("h3")
     
-    n=min(20,len(movies_list))
-    #formatting the list to get the names
+    # The movies list is stored in a ul
+    movies_ul = soup.find("ul", attrs={"role": "presentation"})
+    
+    if movies_ul == None:
+        return {}
+    
+    # h3 tags contain the names of the movies
+    movies_list = movies_ul.select("h3")
+
+    n = min(20, len(movies_list))
+    
+    # formatting the list to get the names
     _movie_list = [movies_list[i].text.split()[1:] for i in range(0, n)]
     actual_movie_list = [" ".join(_movie_list[i]) for i in range(0, n)]
-    
+
     # 'a' tags contain the link to the movies and there are two a tags for each movie in the html code.
     movie_links = movies_ul.select("a")
+    
     # formatting the list to get the links to the movies
-    _movie_links = [(imdb + movie_links[i]["href"]).split("/") for i in range(0, 2*n, 2)]
+    _movie_links = [
+        (imdb + movie_links[i]["href"]).split("/") for i in range(0, 2 * n, 2)
+    ]
     actual_movie_links = ["/".join(movie[: len(movie) - 1]) for movie in _movie_links]
-    
-    movies={}
-    
-    
+
+    movies = {}
+
     for i in range(n):
         movies[actual_movie_links[i]] = actual_movie_list[i]
     return movies
@@ -70,32 +75,41 @@ def get_movie(movie_link):
     r = requests.get(url=movie_link, headers=headers)
     soup = bs(r.content, "html.parser")
     r.close()
-    
+
     # Find the div containing movie details
-    movie_ = soup.find("div", attrs={"class": "sc-69e49b85-0 jqlHBQ"})
+    movie_name = soup.find("h1", attrs={"data-testid": "hero__pageTitle"})
+    movie_year_ = movie_name.find_next_sibling("ul")
+    
     # Try to find the release year of the movie, If the year is not found, set it as -1
     try:
-        year = int(movie_.find("a").text)
+        movie_year=movie_year_.find('a')
+        year = int(movie_year.text)
     except AttributeError:
         year = -1
-        
+
     # Try to find the rating, If the rating is not found, set it as -1
-    rating = soup.find("span", attrs={"class": "sc-bde20123-1 cMEQkK"})
-    try:    
+
+    rating = soup.find(
+        "div", attrs={"data-testid": "hero-rating-bar__aggregate-rating__score"}
+    )
+    try:
+        rating = rating.find("span")
         rating = float(rating.text)
     except AttributeError:
         rating = -1
-    
+
     # Find the div containing the director's details
-    director_div = soup.find("div", attrs={"class": "sc-69e49b85-3 dIOekc"})
-    director_li = director_div.find("li")
-    
+    director_div = soup.find(
+        "div", attrs={"class": "ipc-metadata-list-item__content-container"}
+    )
+    # director_li = director_div.find("li")
+
     # Find all list items containing the directors' name
-    directors_list = director_li.find_all("li")
-    
+    directors_list = director_div.find_all("li")
+
     # Create a list of director's names
     directors = [director.text for director in directors_list]
-    
+
     cast_list = []
     # Find the div containing the cast details
     cast_soup = soup.find(
@@ -104,7 +118,7 @@ def get_movie(movie_link):
             "class": "ipc-sub-grid ipc-sub-grid--page-span-2 ipc-sub-grid--wraps-at-above-l ipc-shoveler__grid"
         },
     )
-    #cast_members contains the list of all the members, we will just extract the name
+    # cast_members contains the list of all the members, we will just extract the name
     try:
         cast_members = cast_soup.contents
         for i in range(len(cast_members)):
@@ -113,7 +127,7 @@ def get_movie(movie_link):
     except:
         # If no cast details are found, leave the cast list empty
         cast_list = []
-        
+
     movie = {}
     movie["rating"] = rating
     movie["release_year"] = year
@@ -127,21 +141,20 @@ def get_reviews(movie_link):
     r = requests.get(url=movie_link + url, headers=headers)
     soup = bs(r.content, "html.parser")
     r.close()
-    
-    #all reviews are present in the following div container.
+
+    # all reviews are present in the following div container.
     reviews_elements = soup.find("div", attrs={"class": "lister-list"}).contents
 
     n = min(
         10,
         len(reviews_elements) / 2,
     )
-    
+
     reviews = []
     for i in range(1, int(2 * n), 2):
-
         reviews_dict = {}
         # Extracting all the relevant info from them
-        
+
         review_ratings_elements = reviews_elements[i].find(
             "span", attrs={"class": "rating-other-user-rating"}
         )
@@ -149,35 +162,29 @@ def get_reviews(movie_link):
         review_dates_elements = reviews_elements[i].find(
             "span", attrs={"class": "review-date"}
         )
-        review_title_elements = reviews_elements[i].find(
-            "a", attrs={"class": "title"}
-        )
+        review_title_elements = reviews_elements[i].find("a", attrs={"class": "title"})
         review_text_elements = reviews_elements[i].find(
-            "div",
-            attrs={"class": "text show-more__control"}
+            "div", attrs={"class": "text show-more__control"}
         )
-        
-        #Ratings are sometimes not available with Review, if not found set it to -1
+
+        # Ratings are sometimes not available with Review, if not found set it to -1
         try:
             rating_html = review_ratings_elements.find("span")
             reviews_dict["rating"] = int(rating_html.text)
         except AttributeError:
             reviews_dict["rating"] = -1
-        
-        #Dates, and review text are always there so, no need to check if they are present or not.
-        
+
+        # Dates, and review text are always there so, no need to check if they are present or not.
+
         date_html = review_dates_elements.text
         reviews_dict["date"] = date_html
-        
-        review_title=review_title_elements.text
-        reviews_dict['title'] =review_title
-        
+
+        review_title = review_title_elements.text
+        reviews_dict["title"] = review_title
+
         reviews_text = review_text_elements.text
         reviews_dict["text"] = reviews_text
-        
+
         reviews.append(reviews_dict)
-        
 
     return reviews
-
-# get_movies("https://www.imdb.com/search/title/?title_type=feature&genres=western")
